b2fd481e3f0f848405ff593e0dd7ad32872a0a85
Frederick Hewett <fred.hewett@gmail.com>
2015-04-20 10:49:37 -0400

    stuff

--- polypoints/main.swift
+++ polypoints/main.swift
@@ -8,5 +8,52 @@
 
 import Foundation
 
-println("Hello, World!")
+let header:String = "shape_id,shape_pt_lat,shape_pt_lon,shape_pt_sequence"
+
+struct RouteShape {
+    let encoded: String
+    let shapeId: String
+}
+
+
+let shapes: [RouteShape] = [
+    RouteShape(
+        encoded: "_kr|Fhd|mL`@|An\\bQtMlRbDxCbGBv|DuqBbmE_eD`t@sc@pZ{Mv]_HrEjD",
+        shapeId: "SH_FAL_OB"
+    ),
+    RouteShape(
+        encoded: "wyl|FtzinLlDjH`ViErXgSb^sg@`Uac@b]c|@|_@sgBxl@woDve@mm@p[gJ~d@wBn{@zK`SfJzErGxE~T",
+        shapeId: "SH_WH_VH"
+    ),
+    RouteShape(
+        encoded: "q|e}Fp}|kLbA_@hAyA~DiCzH{AhB_BdFqIpJmMhHiHlMMb]nFpWdGp[pRhh@fsArPlLzUvB`YdQh^~[j`R`d`@znAzwCz}E~`K",
+        shapeId: "SH_HY_OB"
+    ),
+    RouteShape(
+        encoded: "giy|FdwzrLxOas@hVwXtd@{Zvd@Q~oMnaApoGd_@zMaHt}@cjDlzFqlsAL{hCwR_iD}t@e{Fu{L_vj@q]apDr@qzCzMc|@bc@kp@tzAs{@xEjDdFtHr@mA",
+        shapeId: "SH_QU_OB"
+    ),
+    RouteShape(
+        encoded: "qyl|FvyinLfBvBjBPvV}FfVgSv^wv@l]soAbJck@z`@{~Cpc@qbCln@_uB|c@iw@xqA_r@hZoFvBr@",
+        shapeId: "SH_WH_OB"
+    )
+]
+
+func main() {
+    println("\(header)")
+    
+    for s in shapes {
+        let p: Polyline = Polyline(encodedPolyline: s.encoded, encodedLevels: nil)
+        let c = p.coordinates
+        for (var i=1; i <= c.count; ++i) {
+            let point = c[i-1]
+            println("\(s.shapeId),\(point.latitude),\(point.longitude),\(i)")
+        }
+    }
+}
+
+
+
+
+main()
 
