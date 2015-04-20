31bcd4890981888033b9345959abf677bb817f65
Frederick Hewett <fred.hewett@gmail.com>
2015-04-20 09:39:59 -0400

    Initial Commit

---
+++ polypoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  polypoints
+//
+//  Created by Fred Hewett on 4/20/15.
+//  Copyright (c) 2015 Frederick Hewett. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
